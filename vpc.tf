88b5d590c5158c839b82d9d3c8bcce0d6a808ba6
Dolapo832 <Dolapo832@gmail.com>
2024-04-02 02:02:55 -0400

    adding modified files

--- vpc.tf
+++ vpc.tf
@@ -79,19 +79,19 @@ resource "aws_eip" "nat-eip" {
 }
 
 #Create a NAT Gateway
-resource "aws_nat_gateway" "nat-gateway" {
-  allocation_id = aws_eip.nat-eip.id
-  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
-  tags = {
-      Name = "nat-gateway"
-      }
-}
+# resource "aws_nat_gateway" "nat-gateway" {
+#   allocation_id = aws_eip.nat-eip.id
+#   subnet_id      = element(aws_subnet.public_subnets[*].id)
+#   tags = {
+#       Name = "nat-gateway"
+#       }
+# }
 
-resource "aws_route" "nat_gateway_route" {
-  route_table_id         =  aws_route_table.private_route.id # Specify the ID of your route table
-  destination_cidr_block = "0.0.0.0/0"     # Route all internet-bound traffic
-  nat_gateway_id         = aws_nat_gateway.nat-gateway.id  # Specify the ID of your NAT Gateway
-}
+# resource "aws_route" "nat_gateway_route" {
+#   route_table_id         =  aws_route_table.private_route.id # Specify the ID of your route table
+#   destination_cidr_block = "0.0.0.0/0"     # Route all internet-bound traffic
+#   nat_gateway_id         = aws_nat_gateway.nat-gateway.id  # Specify the ID of your NAT Gateway
+# }
 
 
 #creating a private route table
