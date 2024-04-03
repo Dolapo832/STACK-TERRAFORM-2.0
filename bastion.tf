e12d206f77ddbeb2f02a13dac4c2bb324f114d1e
Dolapo832 <Dolapo832@gmail.com>
2024-04-03 01:35:55 -0400

    adding modified files

--- bastion.tf
+++ bastion.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "bastion-server" {
   ami                     = data.aws_ami.stack_ami.id
   instance_type           = var.instance_type
   vpc_security_group_ids  = [aws_security_group.stack-sg.id]
+  associate_public_ip_address = true
   #user_data               = data.template_file.blogbootstap.rendered
   key_name                = aws_key_pair.Stack_KP.key_name
   subnet_id = element(aws_subnet.public_subnets.*.id, count.index)
