9688da1f4377872c63248e9af120bfe630a06fe4
Dolapo832 <Dolapo832@gmail.com>
2024-04-03 17:42:41 -0400

    adding modified files

--- efs.tf
+++ efs.tf
@@ -1,13 +1,14 @@
-# resource "aws_efs_file_system" "foo" {
-#   creation_token = "my-efs"
-#   tags = {
-#     Name = "MyProduct"
-#   }
-# }
+resource "aws_efs_file_system" "foo" {
+  creation_token = "my-efs"
+  tags = {
+    Name = "MyProduct"
+  }
+}
 
 
-# resource "aws_efs_mount_target" "alpha" {
-#   file_system_id =  aws_efs_file_system.foo.id
-#   avaliability_zone
-#   security_groups = [aws_security_group.db-sg.id]
-# }
+resource "aws_efs_mount_target" "alpha" {
+  count = length(var.public_subnet_cidrs)
+  file_system_id =  aws_efs_file_system.foo.id
+  subnet_id = element(aws_subnet.public_subnets.*.id, count.index)
+  security_groups = [aws_security_group.stack-sg.id]
+}
