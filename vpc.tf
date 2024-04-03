48be549c2df8577a0480c1a675addc0c57e5fba4
Dolapo832 <Dolapo832@gmail.com>
2024-04-03 02:26:54 -0400

    adding modified files

--- vpc.tf
+++ vpc.tf
@@ -80,6 +80,7 @@ resource "aws_route_table_association" "private_subnet_asso" {
 
 #Create an EIP for the NAT-gateway 
 resource "aws_eip" "nat-eip" {
+  count = length(var.public_subnet_cidrs)
    tags = {
       Name = "nat-eip"
       }
@@ -87,7 +88,8 @@ resource "aws_eip" "nat-eip" {
 
 #Create a NAT Gateway
 resource "aws_nat_gateway" "nat-gateway" {
-  allocation_id = aws_eip.nat-eip.id
+  count = length(var.public_subnet_cidrs)
+  allocation_id = aws_eip.nat-eip[0].id
   subnet_id =aws_subnet.public_subnets[0].id
   tags = {
       Name = "nat-gateway"
