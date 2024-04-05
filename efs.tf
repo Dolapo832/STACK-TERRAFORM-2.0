f3d08b8644769408eda87dc38f2a22dc37494ecd
Dolapo832 <Dolapo832@gmail.com>
2024-04-05 02:01:14 -0400

    modified files

--- efs.tf
+++ efs.tf
@@ -8,11 +8,15 @@ resource "aws_efs_file_system" "foo" {
 
 resource "aws_efs_mount_target" "alpha" {
   file_system_id =  aws_efs_file_system.foo.id
-  subnet_id      =  [
-    element(aws_subnet.private_subnets1.*.id, 0),
-    element(aws_subnet.private_subnets2.*.id, 0)
-  ]
+  subnet_id      = element(aws_subnet.private_subnets1.*.id, 0)
   security_groups = [aws_security_group.private-sg.id]
+}
+
+resource "aws_efs_mount_target" "beta" {
+  file_system_id =  aws_efs_file_system.foo.id
+  subnet_id      = element(aws_subnet.private_subnets2.*.id, 0)
+  security_groups = [aws_security_group.private-sg.id]
+  
 }
 
 #blog's configuration
