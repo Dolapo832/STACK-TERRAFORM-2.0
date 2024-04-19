9ef55b0201113ad0fb97cf59eac3bd45bf37cb10
Dolapo832 <Dolapo832@gmail.com>
2024-04-19 15:32:01 -0400

    modified files

--- ecs-cluster.tf
+++ ecs-cluster.tf
@@ -3,35 +3,6 @@ resource "aws_ecs_cluster" "Stack_cluster_Clixx" {
   name = "Clixx-ecs-cluster"
 }
 
-resource "aws_ecs_capacity_provider" "ecs_capacity_provider" {
- name = "test1"
-
- auto_scaling_group_provider {
-   auto_scaling_group_arn = aws_autoscaling_group.app_asg.arn
-
-   managed_scaling {
-     maximum_scaling_step_size = 1000
-     minimum_scaling_step_size = 1
-     status                    = "ENABLED"
-     target_capacity           = 3
-   }
- }
-}
-
-resource "aws_ecs_cluster_capacity_providers" "example" {
- cluster_name = aws_ecs_cluster.Stack_cluster_Clixx.name
-
- capacity_providers = [aws_ecs_capacity_provider.ecs_capacity_provider.name]
-
- default_capacity_provider_strategy {
-   base              = 1
-   weight            = 100
-   capacity_provider = aws_ecs_capacity_provider.ecs_capacity_provider.name
- }
-}
-
-
-
 ## Creating the IAM Role for ECS ###
 resource "aws_iam_role" "ecsTaskExecutionRole2" {
   name               = "ecsTaskExecutionRole2"
@@ -90,11 +61,6 @@ resource "aws_ecs_service" "ecs_service" {
 
  triggers = {
    redeployment = timestamp()
- }
-
- capacity_provider_strategy {
-   capacity_provider = aws_ecs_capacity_provider.ecs_capacity_provider.name
-   weight            = 100
  }
 
  load_balancer {
